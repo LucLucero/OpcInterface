2e2682419defe14ff89fdd4647477ecf79d77156
LucLucero <lucianoxlucero@hotmail.com>
2023-07-14 05:37:07 -0300

    First commit

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import HomePage from '../views/HomePage';
+
+// Configuração das rotas
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomePage,
+    navigationOptions: {
+      title: 'Home',
+    },
+  },
+  // Adicione outras rotas aqui, se necessário
+});
+
+export default AppNavigator;
